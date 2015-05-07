11f3e42580429f6676f38e52849d5532f26d729c
Nikos Kon <nikoskontothanasis@users.noreply.github.com>
2015-05-07 14:24:29 +0300

    Create battelship_C.c

---
+++ battelship_C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+const size=10;//megethos tou pinaka
+int main ()
+{
+	char pin_user [size][size];
+	char pin_pc [size][size];
+	int ship_user=0, ship_pc=0, tries_user=0, tries_pc=0, ep_tries=0;
+
+	printf("Dose ta ploia tou stolou sou:");
+	scanf("%d", &ship_user);
+	while (ship_user<=1 && ship_user>=100)
+	{
+		printf("Lathos! Prospathise ksana!");
+		scanf("%d",&ship_user);
+	}
+
+return 0;
+}
